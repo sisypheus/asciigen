80a3aa7a8b1e58e84b05d4b7d443075d89067aee
Theo <poette.theo@gmail.com>
2024-10-31 22:36:59 +0100

    FEAT: first version with ascii art generation, lacks optimization for large files

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"image"
 	_ "image/jpeg"
-	"image/png"
+	_ "image/png"
 	"log"
+	"math"
 	"os"
 )
 
@@ -16,7 +16,8 @@ func main() {
 	input_path := flag.String("i", "", "Path to the input image")
 	output_path := flag.String("o", "output.txt", "Path to the output ASCII file")
 
-	fmt.Println(*input_path)
+	flag.Parse()
+
 	if *input_path == "" {
 		log.Fatal("Please provide an input image path using -i flag.")
 	}
@@ -32,19 +33,36 @@ func main() {
 	img, _, err := image.Decode(file)
 
 	if err != nil {
-		log.Fatal("Could not open the ")
+		log.Fatal("Could not decode the specified image")
 	}
 
-	grayscaled := rgba_to_gray(img)
+	grayscale_img := rgba_to_gray(img)
 
-	outfile, err := os.Create(*output_path)
+	brightness_map := map_chars_to_brightness(ASCII_CHARS, math.MaxUint8)
+
+	ascii_art := gray_to_ascii(grayscale_img, brightness_map[:])
+
+	output_file, err := os.Create(*output_path)
 
 	if err != nil {
 		log.Fatal("Could not create output file" + err.Error())
-	} 
+	}
 
-	defer outfile.Close()
-	png.Encode(outfile, grayscaled)
+	defer output_file.Close()
+
+	output_file.WriteString(ascii_art)
+}
+
+func map_chars_to_brightness(chars string, max uint8) [256]rune {
+	var brightness_table [256]rune
+	options_len := len(chars)
+
+	for brightness := 0; brightness < int(max)+1; brightness++ {
+		char := int(float64(brightness) / float64(max) * float64(options_len-1))
+		brightness_table[brightness] = rune(chars[char])
+	}
+
+	return brightness_table
 }
 
 func rgba_to_gray(img image.Image) *image.Gray {
@@ -59,4 +77,19 @@ func rgba_to_gray(img image.Image) *image.Gray {
 		}
 	}
 	return gray
+}
+
+func gray_to_ascii(img *image.Gray, mapped_chars []rune) string {
+	var ascii_result string
+	var bounds = img.Bounds()
+
+	for y := 0; y < bounds.Max.Y; y += 2 {
+		for x := 0; x < bounds.Max.X; x++ {
+			var pixel = img.GrayAt(x, y)
+			ascii_result += string(mapped_chars[pixel.Y])
+		}
+		ascii_result += "\n"
+	}
+
+	return ascii_result
 }
